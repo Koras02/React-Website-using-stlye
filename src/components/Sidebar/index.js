03a30db1c4d09c055d1813bd6a76ca531e262b5e
Koras02 <wjdgh0727@gmail.com>
2020-11-25 22:57:10 +0900

    3:12:21

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaBars } from 'react-icons/fa';
 import {SidebarContainer, Icon, CloseIcon, SidebarWrapper,SidebarMenu, SidebarLink,SideBtnWrap,SidebarRoute} from './SidebarElements';
 
 
